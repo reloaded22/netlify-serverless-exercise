c36814e2b2753325d7757936e406b6d5bec710bb
reloaded22 <andres.rojas.talent@gmail.com>
2022-10-21 17:09:36 -0500

    axios error test 3

--- netlify/functions/pokemon.js
+++ netlify/functions/pokemon.js
@@ -4,10 +4,9 @@ exports.handler = async function () {
   const POKE_API = "https://pokeapi.co/api/v2/pokemon/ditto";
 
   const response = await axios.get(POKE_API);
-  const data = await response;
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify(response),
   };
 }
